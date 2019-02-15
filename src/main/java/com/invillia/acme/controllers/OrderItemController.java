dfc46695ca6d1cb891d5a8779872c05dd5e05359
bruno.alex <bruno.alex@ARANOT059.castmeta.local>
2019-02-15 16:09:39 -0200

    Criação do controller de itens da ordem.

---
+++ src/main/java/com/invillia/acme/controllers/OrderItemController.java
@@ -0,0 +1,39 @@
+package com.invillia.acme.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.invillia.acme.entities.OrderItem;
+import com.invillia.acme.repository.OrderItemRepository;
+import com.invillia.acme.repository.OrderRepository;
+
+@RestController
+public class OrderItemController {
+	
+	@Autowired
+	private OrderItemRepository orderItemrepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@GetMapping("/order/{orderId}/items")
+	public Iterable<OrderItem> retrieveAllOrderItemsByOrderId(@PathVariable Long orderId) {
+		return orderItemrepository.findByOrderId(orderId);
+	}
+	
+	@PostMapping("/order/{orderId}/items")
+	public OrderItem addOrderItemToOrder(@PathVariable Long orderId, @RequestBody OrderItem orderItem) throws Exception {
+		return orderRepository.findById(orderId)
+				.map(order -> {
+					orderItem.setOrder(order);
+					return orderItemrepository.save(orderItem);
+				}).orElseThrow(() -> 
+					new Exception("Order informada não foi encontrada")
+				);
+	}
+	
+}
