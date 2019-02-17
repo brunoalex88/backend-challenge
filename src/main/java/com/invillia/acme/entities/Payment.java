35b3c3b42a1105a8a4e63d4c8139145cb64229ab
Bruno Alex de Oliveira <brunoalex_88@yahoo.com.br>
2019-02-17 20:59:54 -0300

    Criação da entidade.

--- src/main/java/com/invillia/acme/entities/Payment.java
+++ src/main/java/com/invillia/acme/entities/Payment.java
@@ -2,11 +2,53 @@ package com.invillia.acme.entities;
 
 import java.util.Date;
 
-public class Payment {
-	private StatusEnum status;
-	private String cardNumber;
-	private Date paymentDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class Payment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="PAYMENT_STATUS")
+	@Enumerated(EnumType.STRING)
+	private StatusEnum status;
+	
+	@Column(name="PAYMENT_CARD_NUMBER", length=20)
+	@NotBlank
+	private String cardNumber;
+	
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	@Column(name="PAYMENT_DATE")
+	@NotNull
+	private Date paymentDate;
+	
+	@OneToOne()
+	@JoinColumn(name="ORDER_ID")
+	private Order order;
+	
+
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public StatusEnum getStatus() {
 		return status;
 	}
@@ -29,6 +71,14 @@ public class Payment {
 
 	public void setPaymentDate(Date paymentDate) {
 		this.paymentDate = paymentDate;
+	}
+	
+	public Order getOrder() {
+		return order;
+	}
+	
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
 }
