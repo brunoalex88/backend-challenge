5e4bc51ffa38d2df3f34cd69e721d546fcc748dd
Bruno Alex de Oliveira <brunoalex_88@yahoo.com.br>
2019-02-17 21:00:29 -0300

    Adicionado validações

--- src/main/java/com/invillia/acme/entities/Store.java
+++ src/main/java/com/invillia/acme/entities/Store.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity(name = "TB_STORE")
 public class Store {
@@ -13,10 +14,12 @@ public class Store {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "store_name")
+	@Column(name = "store_name", length=50)
+	@NotBlank
 	private String name;
 	
-	@Column(name = "store_address")
+	@Column(name = "store_address", length=150)
+	@NotBlank
 	private String address;
 
 	public Long getId() {
