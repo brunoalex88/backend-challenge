53d7e97f1ca8da423a2f504147525e098d48d337
Bruno Alex de Oliveira <brunoalex_88@yahoo.com.br>
2019-02-17 21:01:29 -0300

    Exceção customizada para operações crud.

---
+++ src/main/java/com/invillia/acme/exceptions/CrudException.java
@@ -0,0 +1,10 @@
+package com.invillia.acme.exceptions;
+
+public class CrudException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public CrudException(String message) {
+		super(message);
+	}
+	
+}
